f56160f10a73ad17f6a634fdd7265335f434e52e
yash070504 <yashiitg376@gmail.com>
2024-06-25 16:41:55 +0530

    ready

--- src/app/api/apiSlice.jsx
+++ src/app/api/apiSlice.jsx
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://zcoderbackend-api.onrender.com",
+  baseUrl: "https://zcoderbackendyash-api.onrender.com",
   credentials: "include",
   prepareHeaders: (header, { getState }) => {
     const token = getState().auth.token;
