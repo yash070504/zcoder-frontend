88654b9ae4ed6e28b57b9674c8a3d0254de2d391
yash070504 <140951479+yash070504@users.noreply.github.com>
2025-09-23 23:58:46 +0530

    Delete backend directory

--- backend/routes/commentRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router();
-const {getAllPosts ,createPost ,getPost ,commentOnPost ,getComment} = require("../controller/postController")
-
-router.route("/")
-      .post(commentOnPost)
-      
-
-module.exports = router
